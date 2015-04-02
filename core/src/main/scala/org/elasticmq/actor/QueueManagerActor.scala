0be22e42a24145c07fb71bd575243e68977128ff
Daniel Hochman <dhochman@lyft.com>
2015-04-01 18:22:16 -0700

    get or create queue when looking up

--- core/src/main/scala/org/elasticmq/actor/QueueManagerActor.scala
+++ core/src/main/scala/org/elasticmq/actor/QueueManagerActor.scala
@@ -1,18 +1,23 @@
 package org.elasticmq.actor
 
 import org.elasticmq.msg._
+import org.joda.time.{DateTime, Duration}
 import scala.reflect._
 import org.elasticmq.msg.DeleteQueue
 import org.elasticmq.msg.CreateQueue
 import akka.actor.{Props, ActorRef}
 import org.elasticmq.util.{Logging, NowProvider}
 import org.elasticmq.actor.queue.QueueActor
-import org.elasticmq.QueueAlreadyExists
+import org.elasticmq.{MillisVisibilityTimeout, QueueData, QueueAlreadyExists}
 import org.elasticmq.actor.reply._
 
 class QueueManagerActor(nowProvider: NowProvider) extends ReplyingActor with Logging {
   type M[X] = QueueManagerMsg[X]
   val ev = classTag[M[Unit]]
+
+  val DefaultVisibilityTimeout = 30L
+  val DefaultDelay = 0L
+  val DefaultReceiveMessageWaitTimeSecondsAttribute = 0L
 
   private val queues = collection.mutable.HashMap[String, ActorRef]()
 
@@ -20,7 +25,10 @@ class QueueManagerActor(nowProvider: NowProvider) extends ReplyingActor with Log
     case CreateQueue(queueData) => {
       if (queues.contains(queueData.name)) {
         logger.debug(s"Cannot create queue, as it already exists: $queueData")
-        Left(new QueueAlreadyExists(queueData.name))
+//        Left(new QueueAlreadyExists(queueData.name))
+
+        //TODO flag for unsafe mode
+        Right(queues.get(queueData.name).getOrElse(context.actorOf(Props(new QueueActor(nowProvider, queueData)))))
       } else {
         logger.info(s"Creating queue $queueData")
         val actor = context.actorOf(Props(new QueueActor(nowProvider, queueData)))
@@ -35,8 +43,17 @@ class QueueManagerActor(nowProvider: NowProvider) extends ReplyingActor with Log
     }
 
     case LookupQueue(queueName) => {
-      val result = queues.get(queueName)
+      var result = queues.get(queueName)
       logger.debug(s"Looking up queue $queueName, found?: ${result.isDefined}")
+
+      // TODO flag for unsafe mode
+      if (!result.isDefined) {
+        val queueData = QueueData(queueName, MillisVisibilityTimeout(1L), Duration.ZERO, Duration.ZERO, DateTime.now(), DateTime.now())
+        val actor = context.actorOf(Props(new QueueActor(nowProvider, queueData)))
+        queues(queueData.name) = actor
+        result = queues.get(queueData.name)
+      }
+
       result
     }
 
